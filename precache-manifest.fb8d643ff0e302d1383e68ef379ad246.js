3d8c4c11fe8773b8c59888de2563e346edf3f0e1
dey-subhajit <subhajit.dey1998@yahoo.com>
2020-12-06 20:31:52 +0530

    Updates

---
+++ precache-manifest.fb8d643ff0e302d1383e68ef379ad246.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6af68e958ab1d1a170bedaea3990368",
+    "url": "/weather-forcust/index.html"
+  },
+  {
+    "revision": "c98b9e95908ebe97bbae",
+    "url": "/weather-forcust/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "55d654f4109f6d2b976f",
+    "url": "/weather-forcust/static/css/main.41e10f3a.chunk.css"
+  },
+  {
+    "revision": "c98b9e95908ebe97bbae",
+    "url": "/weather-forcust/static/js/2.cd2a5b41.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/weather-forcust/static/js/2.cd2a5b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d654f4109f6d2b976f",
+    "url": "/weather-forcust/static/js/main.c583138f.chunk.js"
+  },
+  {
+    "revision": "eda70599101d92efa15c",
+    "url": "/weather-forcust/static/js/runtime-main.f7f4312f.js"
+  },
+  {
+    "revision": "51be6a4d74669f751b0d3f4f13515045",
+    "url": "/weather-forcust/static/media/bg.51be6a4d.jpg"
+  },
+  {
+    "revision": "da5f15a5fdffc9b7f1c85df5b23bc4ee",
+    "url": "/weather-forcust/static/media/img1.da5f15a5.jpg"
+  }
+]);
